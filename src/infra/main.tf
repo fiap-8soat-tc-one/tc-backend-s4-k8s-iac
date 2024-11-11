0bb72a81f8f71e2c9019af02fe179a0cd00720fa
c_jean.silva <c_jean.silva@hapvida.com.br>
2024-11-11 18:53:09 -0300

    feat: add remote state s3

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -1,22 +1,23 @@
 # Specify the AWS provider
 provider "aws" {
-  region = "us-east-1" # Replace with your preferred AWS region
+  region = "us-east-1"  # Substitua pela região preferida, se necessário
 }
 
-# Generate a random string for unique bucket name (optional)
-resource "random_id" "bucket_id" {
-  byte_length = 4
+# Configure o backend para armazenar o estado no bucket S3 especificado
+terraform {
+  backend "s3" {
+    bucket = "fiap-2bdc9c48"  # Nome do bucket onde o estado será armazenado
+    key    = "terraform/states/team32.tfstate"  # Caminho do arquivo de estado dentro do bucket
+    region = "us-east-1"  # Região do bucket
+  }
 }
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "fiap-${random_id.bucket_id.hex}"  # Replace with a unique name
+  bucket = "fiap-8soat-team32-s3"  # Substitua com o nome desejado do bucket
 }
 
 # Output the bucket name
 output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
-
-# Random ID provider to generate the unique identifier
-provider "random" {}
